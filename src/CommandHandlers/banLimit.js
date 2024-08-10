c7c9577298c439d1e695c97261033bea989c0f16
S4LTER <kros.krosov@inbox.ru>
2024-08-11 00:26:18 +0600

    subject of `else if` has been fully explored

--- src/CommandHandlers/banLimit.js
+++ src/CommandHandlers/banLimit.js
@@ -10,27 +10,24 @@ import logger from '../logger.js';
 export default (bot, db) => {
   bot.command('ban_limit', isAdminGuard, async (ctx) => {
     const match = ctx.match.trim();
+    const num = parseFloat(match);
 
     if (match === '') {
       replyToMember(ctx, 'Введите целое число от 1 до 60 включительно.');
     } else if (match.split(' ').length > 1) {
       replyToMember(ctx, 'Вы указали слишком много аргументов.');
-    } else if (Number.isNaN(parseFloat(match))) {
+    } else if (Number.isNaN(num)) {
       replyToMember(ctx, 'Введите целое число.');
+    } else if (!Number.isInteger(num)) {
+      replyToMember(ctx, 'Введите целое число.');
+    } else if (num < 1) {
+      replyToMember(ctx, 'Введите число больше или равное 1.');
+    } else if (num > 60) {
+      replyToMember(ctx, 'Введите число меньше или равное 60.');
     } else {
-      const num = parseFloat(match);
-
-      if (!Number.isInteger(num)) {
-        replyToMember(ctx, 'Введите целое число.');
-      } else if (num < 1) {
-        replyToMember(ctx, 'Введите число больше или равное 1.');
-      } else if (num > 60) {
-        replyToMember(ctx, 'Введите число меньше или равное 60.');
-      } else {
-        replyToMember(ctx, `Успешно установлен бан-лимит ${num} ${getNoun(num, 'минута', 'минуты', 'минут')}.`);
-        db.setStayInChatLimit(ctx.chat.id, match);
-        logger.info('ban_limit', `In chat ${ctx.chat.id} set ban limit ${match}`);
-      }
+      replyToMember(ctx, `Успешно установлен бан-лимит ${num} ${getNoun(num, 'минута', 'минуты', 'минут')}.`);
+      db.setStayInChatLimit(ctx.chat.id, match);
+      logger.info('ban_limit', `In chat ${ctx.chat.id} set ban limit ${match}`);
     }
   });
 };
