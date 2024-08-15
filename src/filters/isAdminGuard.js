37b503052d093b4a896a3b3220541299545177ac
S4LTER <kros.krosov@inbox.ru>
2024-08-16 03:41:53 +0600

    added restrict rights check

--- src/filters/isAdminGuard.js
+++ src/filters/isAdminGuard.js
@@ -1,7 +1,16 @@
-import { guard, isAdmin } from 'grammy-guard';
+import { guard } from 'grammy-guard';
 
-export default guard(isAdmin, (ctx) => {
-  ctx.reply('Данная команда доступна только администраторам.', {
+/**
+ * @param { import('grammy').Context }
+ */
+const isAdminWithRestrictRights = async (ctx) => {
+  const member = await ctx.getChatMember(ctx.from.id);
+
+  return (member.status === 'administrator' && member.can_restrict_members) || member.status === 'creator';
+};
+
+export default guard(isAdminWithRestrictRights, (ctx) => {
+  ctx.reply('Данная команда доступна только администраторам с правом банить участников.', {
     reply_parameters: {
       message_id: ctx.message.message_id,
     },
