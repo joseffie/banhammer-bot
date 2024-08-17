3e2d71c2aa3a6c0487f5a5ca43d3b0c268784a14
S4LTER <kros.krosov@inbox.ru>
2024-08-17 22:55:12 +0600

    corrected `TIME_FRAME` description

--- config.example.js
+++ config.example.js
@@ -4,10 +4,13 @@ export const BOT_TOKEN = 'your-bot-token';
 /** Bot owner ID (you can check with \@raw_data_bot). */
 export const BOT_OWNER_ID = 0;
 
-/** Number of allowed commands during TIME_FRAME. */
+/** Number of allowed commands during `TIME_FRAME`. */
 export const COMMAND_LIMIT = 3;
 
-/** The time in milliseconds during which the user can be muted if he enters more than 1 command. */
+/**
+ * The time in milliseconds during which the user can be muted if he enters more
+ * than `COMMAND_LIMIT` commands.
+ */
 export const TIME_FRAME = 2500;
 
 /** The time in milliseconds, for which the user will be muted for flooding. */
