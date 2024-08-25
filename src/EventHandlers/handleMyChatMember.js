4bc398a561f3568e485e59142bed15dc32270b4e
S4LTER <kros.krosov@inbox.ru>
2024-08-26 03:16:49 +0600

    added `try... catch`, bump patch

--- src/EventHandlers/handleMyChatMember.js
+++ src/EventHandlers/handleMyChatMember.js
@@ -17,17 +17,22 @@ export default (bot) => {
       const chatId = ctx.chat.id;
       logger.warn('rights_taken_away', `I was restricted in chat ${chatId}.`);
 
-      if (await bot.db.hasDefModeEnabled(chatId)) {
-        bot.db.toggleDefMode(chatId, false);
-        logger.warn('def_mode', `Disabled in chat ${chatId} due to restriction me.`);
-        return bot.reply(ctx, {
-          message: 'rights_taken_away_warn',
-          replying: false,
-          options: true,
-        });
-      }
+      try {
+        if (await bot.db.hasDefModeEnabled(chatId)) {
+          return bot.reply(ctx, {
+            message: 'rights_taken_away_warn',
+            replying: false,
+            options: true,
+          }).then(() => {
+            bot.db.toggleDefMode(chatId, false);
+            logger.warn('def_mode', `Disabled in chat ${chatId} due to restriction me.`);
+          });
+        }
 
-      return bot.reply(ctx, 'rights_taken_away_warn', false);
+        return bot.reply(ctx, 'rights_taken_away_warn', false);
+      } catch (error) {
+        console.error('An error in handling my chat member occurred:', error);
+      }
     }
 
     return {};
